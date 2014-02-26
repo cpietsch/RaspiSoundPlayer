8145c5ed0834c1fc4befa683df2fc9b2353f1b57
Christopher Pietsch <cpietsch@gmail.com>
2014-02-26 16:39:04 +0100

    test

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-var Sound = require('node-mpg123');
+//var Sound = require('node-mpg123');
 var serialport = require("serialport");
 var walk    = require('walk');
+var fs = require('fs');
 
-// with ability to pause/resume:
-var music = new Sound('files/test.mp3');
-music.play();
 
-setTimeout(function () {
-    music.pause(); // pause the music after five seconds
-}, 5000);
+// // with ability to pause/resume:
+// var music = new Sound('files/test.mp3');
+// music.play();
 
-setTimeout(function () {
-    music.resume(); // and resume it two seconds after pausing
-}, 7000);
+// setTimeout(function () {
+//     music.pause(); // pause the music after five seconds
+// }, 5000);
 
-// you can also listen for various callbacks:
-music.on('complete',function () {
-    console.log('Done with playback!');
-});
+// setTimeout(function () {
+//     music.resume(); // and resume it two seconds after pausing
+// }, 7000);
+
+// // you can also listen for various callbacks:
+// music.on('complete',function () {
+//     console.log('Done with playback!');
+// });
 
 
 function listFiles(dir){
@@ -29,25 +31,28 @@ function listFiles(dir){
         if (fs.statSync(name).isDirectory()){
             getFiles(name);
         }else{
-            console.log(name);
+            //console.log(name);
             arr.push(name);
         }
     }
     return arr;
 }
 
-var files = listFiles("files");
-console.log(files);
+var mp3 = listFiles("mp3");
+console.log("mp3",mp3);
 
 serialport.list(function (err, ports) {
 	ports.forEach(function(port) {
 	  //console.log("list",port)
-	  var name = port.comName.split(".")[1];
 
-	  if(name.split("-")[0] == "usbserial"){
-	    console.log("found ARDUINO on ", name)
+	  if(port.manufacturer.split("Arduino").length > 1){
 
-	    serialInit(name);
+	  	var arduino = port.comName.split(".")[1];
+	    console.log("found ARDUINO on ", arduino)
+
+	    serialInit(arduino);
+	  } else {
+	  	console.log("NO ARDUINO FOUND!!!");
 	  }
 
 	});
